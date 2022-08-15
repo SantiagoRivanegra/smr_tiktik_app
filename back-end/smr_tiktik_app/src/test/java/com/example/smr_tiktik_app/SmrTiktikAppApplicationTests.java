1a3479380813c4653bf5e1aab42569a3a88bbae5
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-08-15 18:13:42 -0300

    first commit back

---
+++ back-end/smr_tiktik_app/src/test/java/com/example/smr_tiktik_app/SmrTiktikAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smr_tiktik_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmrTiktikAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
