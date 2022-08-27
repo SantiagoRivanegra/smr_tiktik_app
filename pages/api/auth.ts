34f32180cafb10ce053dd207994dc16c167eaf86
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-08-27 16:16:40 -0300

    suggestedAccount

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -2,10 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { client } from '../../utils/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-if(req.method === 'POST') {
-  const user = req.body
+  if(req.method === 'POST') {
+    const user = req.body
 
-  client.createIfNotExists(user)
-    .then(() => res.status(200).json('Login success'))
-}
+    client.createIfNotExists(user)
+      .then(() => res.status(200).json('Login success'))
+  }
 }
