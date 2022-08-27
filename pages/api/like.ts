52e962a797484bcac43ecdb73611fa32505e66cb
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-08-27 15:31:43 -0300

    add comments

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -6,21 +6,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 if(req.method === 'PUT') {
   const { userId, postId, like } = req.body
 
-  const data = like ? await client
-    .patch(postId)
-    .setIfMissing({ likes:[] })
-    .insert('after', 'likes[-1]', [
-      {
-        _key: uuid(),
-        _ref: userId
-      }
-    ])
-    .commit()
-  : await client
-    .patch(postId)
-    .unset([`likes[_ref=="${userId}"]`])
-    .commit()
+  const data = 
+    like ? await client
+      .patch(postId)
+      .setIfMissing({ likes:[] })
+      .insert('after', 'likes[-1]', [
+        {
+          _key: uuid(),
+          _ref: userId
+        }
+      ])
+      .commit()
+    : await client
+      .patch(postId)
+      .unset([`likes[_ref=="${userId}"]`])
+      .commit()
 
-    res.status(200).json(data)
+      res.status(200).json(data)
   }
 }
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -25,6 +25,8 @@ const Detail = ({ postDetails }: IProps) => {
   const videoRef = useRef<HTMLVideoElement>(null)
   const router = useRouter()
   const { userProfile }: any = useAuthStore()
+  const [comment, setComment] = useState('')
+  const [isPostingComment, setIsPostingComment] = useState(false)
 
   const onVideoClick = () => {
     if(playing){
@@ -51,6 +53,23 @@ const Detail = ({ postDetails }: IProps) => {
       })
 
       setPost({ ...post, likes: data.likes })
+    }
+  }
+
+  const addComment = async (e) => {
+    e.prevent.default()
+
+    if(userProfile && comment) {
+      setIsPostingComment(true)
+
+      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile?._id,
+        comment
+      })
+
+      setPost({ ...post, comments: data.comments })
+      setComment('')
+      setIsPostingComment(false)
     }
   }
 
@@ -138,7 +157,11 @@ const Detail = ({ postDetails }: IProps) => {
             )}
           </div>
           <Comments 
-          
+            comment={comment}
+            setComments={setComment}
+            addComment={addComment}
+            comments={post.comments}
+            isPostingComment={isPostingComment}
           />
 
         </div>
